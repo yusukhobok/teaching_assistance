173357567daa5fa945f00e1f65ad141f409de47e
yuri <khvyus@gmail.com>
2020-10-04 12:48:02 +1000

    Add allowed hosts

--- teaching/settings.py
+++ teaching/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '0crwhe!e#3qk)jwl$k2d_ndhue)78b
 DEBUG = False
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'infinite-island-40793.herokuapp.com']
 
 
 # Application definition
